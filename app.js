312b742a2413d122956ccc7714f55dfedf97a4e3
Evans Wanjau <evanswanjau@gmail.com>
2021-06-13 11:31:14 +0300

    Added access control headers

--- app.js
+++ app.js
@@ -6,6 +6,19 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
 data = [{ id: "12345678", title: "hello", post: "hello world" }];
 
 router.get("/", (req, res) => {
